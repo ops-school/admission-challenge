17e218efc6b0f5b051f32ec9d5a6662a16e9e074
MeytarHen <140611040+MeytarHen@users.noreply.github.com>
2023-09-06 17:42:16 +0300

    Update exercise4-fix_server1.sh

--- materials/vagrant/fixScripts/exercise4-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server1.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 #add fix to exercise4-server1 here
+sudo echo "192.168.60.11 server2" /etc/hosts
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sudo systemctl restart sshd
